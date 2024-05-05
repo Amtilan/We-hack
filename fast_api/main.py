c72e7d58c3be4f97e0fadc067e10c00a6bdfa412
Amtilan <149930972+Amtilan@users.noreply.github.com>
2024-05-05 11:29:32 +0500

    second commit

--- fast_api/main.py
+++ fast_api/main.py
@@ -14,3 +14,7 @@ app.add_middleware(
 
 app.include_router(tasks_router)
 
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
